038ff1c7a6b1b7ba89dd7a632871d61fe47b2f32
LIN, YU-TE <eiwterlin@hotmail.com>
2022-05-22 12:59:31 +0700

    main scene add button

--- MMRO2/Scenes/Home.cs
+++ MMRO2/Scenes/Home.cs
@@ -12,8 +12,9 @@ namespace MMRO2.Scenes
 		private Texture2D _backgroundTex;
 		private Texture2D _logoTex;
 
-		private Texture2D _playTex;
-		private Texture2D _quitTex;
+		private Texture2D _buttonTex;
+
+		private SpriteFont _font;
 
 		private Sprites.Rectangle _background;
 		private Sprites.Rectangle _logo;
@@ -30,46 +31,75 @@ namespace MMRO2.Scenes
 			_recList = new List<Sprites.Rectangle>();
 			_buttonList = new List<Sprites.Buttons>();
 
+			//Load Font
+			_font = Global.Instance.Content.Load<SpriteFont>("fonts/font20");
+
 			//Load Texture
 			_backgroundTex = Global.Instance.Content.Load<Texture2D>("images/backgrounds/main");
 			_logoTex = Global.Instance.Content.Load<Texture2D>("images/logo/logo_2");
 
-			//_playTex = Global.Instance.Content.Load<Texture2D>("images/ui/");
-			//_quitTex = Global.Instance.Content.Load<Texture2D>("images/ui/");
+			_buttonTex = Global.Instance.Content.Load<Texture2D>("images/ui/main_button");
 
+			//Create Object
 			_background = new Sprites.Rectangle(_backgroundTex, _backgroundTex.Width, _backgroundTex.Height);
 			_background.Position = new Vector2(Settings.Window.HalfWidth, Settings.Window.HalfHeight);
 
 			_logo = new Sprites.Rectangle(_logoTex, (int)(_logoTex.Width / 1.25), (int)(_logoTex.Height / 1.25));
 			_logo.Position = new Vector2(Settings.Window.HalfWidth, Settings.Window.HalfHeight - 150);
 
+			_playButton = new Sprites.Buttons(_buttonTex, _font, " PLAY", _buttonTex.Width / 3, _buttonTex.Height / 3);
+			_playButton.Position = new Vector2(Settings.Window.HalfWidth, Settings.Window.HalfHeight + 80);
+			_playButton.setTextColor(Color.White);
+
+			_quitButton = new Sprites.Buttons(_buttonTex, _font, " QUIT", _buttonTex.Width / 3, _buttonTex.Height / 3);
+			_quitButton.Position = new Vector2(Settings.Window.HalfWidth, _playButton.Position.Y + 80);
+			_quitButton.setTextColor(Color.White);
+
+			_playButton.Click += _playButton_clicked;
+			_quitButton.Click += _quitButton_clicked;
+
 			_recList.Add(_background);
 			_recList.Add(_logo);
 
+			_buttonList.Add(_playButton);
+			_buttonList.Add(_quitButton);
 
 		}
 
 		public override void Update()
-		{/*
+		{
 			foreach (var button in _buttonList)
 			{
 				button.Update();
-			}*/
+			}
 		}
 
 		public override void Draw()
 		{
 			BeginSprite();
+
 			foreach (var rectangle in _recList)
 			{
 				rectangle.Draw();
 			}
-			EndSprite();
-			/*
+
 			foreach (var button in _buttonList)
 			{
 				button.Draw();
-			}*/
+			}
+
+			EndSprite();
+		}
+
+		public void _playButton_clicked(object sender, EventArgs args)
+		{
+			Global.Instance.GameData.Reset();
+			Utils.Scene.Control.ChangeScene(Enums.Scenes.Playing);
+		}
+
+		public void _quitButton_clicked(object sender, EventArgs args)
+		{
+			Game1.Quit();
 		}
 	}
 }
