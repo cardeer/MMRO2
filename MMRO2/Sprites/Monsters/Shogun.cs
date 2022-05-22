92737c45f46dea7fcfc27a5c9a5c6010aea1d17b
コーヒー <cardeer123_1@hotmail.com>
2022-05-22 19:12:39 +0700

    shogun animation, hitbox

--- MMRO2/Sprites/Monsters/Shogun.cs
+++ MMRO2/Sprites/Monsters/Shogun.cs
@@ -48,7 +48,7 @@ namespace MMRO2.Sprites.Monsters
             Body.IgnoreGravity = true;
             Body.FixedRotation = true;
 
-            Body.CreateRectangle(Width, Height, 1f, Vector2.Zero);
+            Body.CreateRectangle(Width - 5, Height - 3, 1f, Vector2.Zero);
             Body.OnCollision += Body_OnCollision;
         }
 
